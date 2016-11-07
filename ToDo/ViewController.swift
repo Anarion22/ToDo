073ee57a786bcace88265c35eed1df7a097202f4
Matija Sabljak <matijasabljak@Matijas-MacBook-Pro.local>
2016-11-07 12:08:13 +0100

    Added 2nd line of code

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Hello")
+        print("2nd line")
     
         // Do any additional setup after loading the view, typically from a nib.
     }
